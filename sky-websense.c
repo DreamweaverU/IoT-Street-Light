9dafdcdc41cdd28a6a5149978ac6f5ae94210e20
Patrick Hu <ultrahp@gmail.com>
2016-07-11 15:05:42 -0400

    Print last octet of the sender addr

--- sky-websense.c
+++ sky-websense.c
@@ -188,8 +188,18 @@ receiver(struct simple_udp_connection *c,
          const uint8_t *data,
          uint16_t datalen)
 {
-  printf("Data received on port %d from %d:port %d with length %d\n",
-         receiver_port, uip_ipaddr4(sender_addr), sender_port, datalen);
+  printf("Data received on port %d from port %d with length %d\n",
+         receiver_port, sender_port, datalen);
+  print_ipv6_addr(sender_addr);
+  
+}
+
+static void
+print_ipv6_addr(const uip_ipaddr_t *ip_addr) {
+    int i;
+    for (i = 0; i < 16; i++) {
+        printf("%02x", ip_addr->u8[i]);
+    }
 }
 
 /************************************************************
